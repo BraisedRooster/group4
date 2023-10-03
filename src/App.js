736e49ba3771c422b6bd6b02494959c0a8bd1b7f
Boyi <137158557@qq.com>
2023-10-03 17:35:06 +1100

    Change SideBar Position, dark mode, simple guide

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
-import AppHeader from "./components/app_header";
-import AlterImage from "./components/AlterImage";
-import React, { useState } from "react";
-import JoinUsBlock from "./components/JoinUsBlock";
-import LowCarbonBlock from "./components/LowCarbonBlock";
-import AdvantageBlock from "./components/AdvantageBlock";
-import AppBottom from "./components/app-bottom";
-import ModalForm from "./components/ModalForm";
-import rightImg1 from "./rightimg-1.png";
-import rightImg2 from "./rightimg-2.png";
-import rightImg3 from "./righting-3.png";
-import ButtonAppBar from "./components-2/ButtonAppBar";
-import Transaction from "./components-2/Transaction";
-import MyCharger from "./components-2/MyCharger";
-import AdminPage from "./components-admin/AdminPage";
+import LoginForm from "./components/Login/LoginForm";
+import RegisterForm from "./components/Register/RegisterForm";
+import RetrieveForm from "./components/Retrieve/RetrieveForm";
+import AppHeader from "./components/CoverPage/app_header";
+import AlterImage from "./components/CoverPage/AlterImage";
+import React, { useState, useEffect } from "react";
+import JoinUsBlock from "./components/CoverPage/JoinUsBlock";
+import LowCarbonBlock from "./components/CoverPage/LowCarbonBlock";
+import AdvantageBlock from "./components/CoverPage/AdvantageBlock";
+import AppBottom from "./components/CoverPage/app-bottom";
+import ModalForm from "./components/CoverPage/ModalForm";
+import rightImg1 from "./matirial/Image/rightimg-1.png";
+import rightImg2 from "./matirial/Image/rightimg-2.png";
+import rightImg3 from "./matirial/Image/righting-3.png";
+import ButtonAppBar from "./components/MainPage/ButtonAppBar";
+import Transaction from "./components/MainPage/Transaction";
+import MyCharger from "./components/MainPage/MyCharger";
+import AdminPage from "./components/Admin/AdminPage";
+
 import {
   BrowserRouter as Router,
   Route,
@@ -22,8 +26,10 @@ import {
   Outlet,
   useNavigate,
 } from "react-router-dom";
-import HelpModel from "./components-2/HelpModel";
-import AddChargerModel from "./components-2/AddChargerModel";
+
+import HelpModel from "./components/MainPage/HelpModel";
+import AddChargerModel from "./components/MainPage/AddChargerModel";
+import GoogleMapReact from "google-map-react";
 
 function App() {
   return (
@@ -35,6 +41,9 @@ function App() {
           <Route path="/transcationPage" element={<TranscationPage />} />
           <Route path="/myCharger" element={<MyChargerPage />} />
           <Route path="/AdminPage" element={<Adminpage />} />
+          <Route path="/login/" element={<LoginForm />} />
+          <Route path="/register/" element={<RegisterForm />} />
+          <Route path="/retrieve/" element={<RetrieveForm />} />
         </Routes>
       </div>
     </Router>
@@ -52,6 +61,7 @@ function MainPage() {
   const closeDialogEvent = () => {
     setShowDialog(false);
   };
+
   return (
     <div className="App">
       <AppHeader change={showDialogEvent}></AppHeader>
@@ -81,27 +91,51 @@ function MainPage() {
           />
         </div>
       </div>
+
       <JoinUsBlock />
       <LowCarbonBlock />
       <AdvantageBlock />
       <AppBottom />
-      <ModalForm
-        showDialog={showDialog}
-        closeEvent={closeDialogEvent}
-        submit={() => navigate("/MapPage")}
-      />
+      <ModalForm />
     </div>
   );
 }
 
 function MapPage() {
   const navigate = useNavigate();
+
+  const defaultProps = {
+    center: {
+      lat: -33.8688, // Latitude for Sydney
+      lng: 151.2093, // Longitude for Sydney
+    },
+    zoom: 11,
+  };
+
   return (
-    <ButtonAppBar
-      transactionpage={() => navigate("/TranscationPage")}
-      adminpage={() => navigate("/Adminpage")}
-      myChargers={() => navigate("/myCharger")}
-    />
+    <div style={{ position: "relative", height: "100vh", width: "100vw" }}>
+      <ButtonAppBar
+        transactionpage={() => navigate("/TranscationPage")}
+        adminpage={() => navigate("/Adminpage")}
+        myChargers={() => navigate("/myCharger")}
+      />
+
+      <div
+        style={{
+          position: "absolute",
+          top: "10%",
+          left: "10%",
+          height: "80%",
+          width: "80%",
+        }}
+      >
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyDnRpQ2BiFb6skH2qkvgCW0Bthwb83PVf0" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        ></GoogleMapReact>
+      </div>
+    </div>
   );
 }
 
